c67ce2ac2d121e889b41b3056b52d784da612033
Eshwar Lal <151713880+eshwarlal1242@users.noreply.github.com>
2024-06-06 00:46:11 +0500

    Add files via upload
    Javascripts For React
    
    Signed-off-by: Eshwar Lal <151713880+eshwarlal1242@users.noreply.github.com>

---
+++ Variable.js
@@ -0,0 +1,18 @@
+
+
+
+let count = 0;
+count = 1;
+console.log(count);
+
+
+const PI = 3.14;
+console.log(PI);
+
+
+var count1 = 0;
+if(true){
+    count1  = 1;
+    console.log(count1);
+}
+console.log(count1);
